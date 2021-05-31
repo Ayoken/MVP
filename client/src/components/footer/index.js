c77f1d9367e92063d4a3e692d30b85c55444db24
Japhet <akandejaphet@gmail.com>
2021-05-31 19:06:05 +0100

    footer init

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -4,7 +4,52 @@ function Footer() {
   return (
     <div className="App">
       <footer>
-         
+        
+        <div className="footer-logo"> AYOKEN inc all rights reserved</div>
+
+
+         <div className="footer-text"> 
+              <div>
+              <ol> 
+                <li> Resources</li>
+                <li> FAQ</li>
+                <li> Blog</li>
+                <li> The Market</li>
+              </ol>
+                </div>
+
+                <div>
+              <ol> 
+                <li>Social</li>
+                <li> Discord</li>
+                <li> Instagram</li>
+                <li> Twitter</li>
+              </ol>
+                </div>
+
+                <div>
+              <ol> 
+                <li> My Account</li>
+                <li> My Profile</li>
+                <li> My Transactions</li>
+                <li> My Collections</li>
+              </ol>
+                </div>
+
+                <div>
+              <ol> 
+                <li> Privacy</li>
+                <li> Newsletter</li>
+               
+              </ol>
+                </div>
+
+
+
+
+
+</div>
+
       </footer>
     </div>
   );
